4797f764c9284db37c390a7657c27dd005dc43c2
Guillaume <guillaumegirardpro@gmail.com>
2023-10-23 08:36:55 +0200

    work on the csv parser

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::collections::HashMap;
+
 fn main() {
     // tauri::Builder::default()
     //     .run(tauri::generate_context!())
     //     .expect("error while running tauri application");
-    process_file("/home/guillaume/Downloads/2023-10-11-981_segments_export_1697032083.csv");
+    let _ = process_file("/home/yehra/Downloads/convertcsv.csv");
 }
 
-fn process_file(file: &str) -> Result<(), std::io::Error> {
+fn process_file(file: &str) -> Result<(HashMap<&str, Vec<HashMap<usize, &str>>>), std::io::Error> {
     use std::fs::File;
     use std::io::prelude::*;
     use std::io::BufReader;
@@ -17,17 +19,34 @@ fn process_file(file: &str) -> Result<(), std::io::Error> {
     let mut reader = BufReader::new(file).lines();
     let mut string_buf: Vec<String> = Vec::new();
 
-    //TODO: in each line, split by comma, and create a hashmap with the position in list as key and the value as value
-    //Iterator::position() to get the index of the value in the header
-
     while let Some(line) = reader.next() {
         let line = line.unwrap();
         string_buf.push(line);
     }
-    let header_string = string_buf[0].clone();
-    let doc = string_buf[1..].to_vec();
+    let header = string_buf[0].clone();
+    let mut doc = string_buf[1..].to_vec();
+    let mut doc_indexed: Vec<HashMap<usize, &str>> = vec![];
 
-    println!("header: {}", header_string);
-    println!("doc: {:#?}", doc);
-    Ok(())
+    for full_line in doc.iter_mut() {
+        let e = full_line
+            .split(",")
+            .enumerate()
+            .collect::<HashMap<usize, &str>>();
+
+        doc_indexed.push(e.to_owned());
+    }
+
+    let header = header
+        .split(",")
+        .enumerate()
+        .collect::<HashMap<usize, &str>>();
+
+    // println!("header: {:?}", header);
+    // println!("{:#?}", doc_indexed);
+
+    let mut result = HashMap::new();
+    result.insert("header", vec![header]);
+    result.insert("data", doc_indexed.clone());
+
+    Ok(result)
 }
